fab09663f3fddf2b7c56145586ff2ed2089303a5
abangtsy7 <abangtsy5@gmail.com>
2024-12-28 07:56:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Rehan\n' // Nama kamu
+            + 'ORG:REBOT;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 898-050-446-0\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
